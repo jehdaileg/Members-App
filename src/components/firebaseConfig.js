6ceb43ae8ac5040bbad17763bf36dd0795971671
jehdaileg <jehdailegrand12@gmail.com>
2021-08-22 19:30:35 -0900

    Firebase Configuration, Firestore, Auth and other features configuration

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDgjuifIx25Z-rqwp5L4C8u9DBfyt_wZyM",
+    authDomain: "vue-projects-jehd.firebaseapp.com",
+    projectId: "vue-projects-jehd",
+    storageBucket: "vue-projects-jehd.appspot.com",
+    messagingSenderId: "902812106392",
+    appId: "1:902812106392:web:1e75bbd46c17ff15cedaed"
+  };
